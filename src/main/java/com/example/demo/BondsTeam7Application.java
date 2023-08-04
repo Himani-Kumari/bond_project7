4b13beea3f7145821f76de2f2e9ae7ecdeb997ef
Himani Kumari <hsagg2000@gmail.com>
2023-08-04 23:49:47 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/BondsTeam7Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondsTeam7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondsTeam7Application.class, args);
+	}
+
+}
